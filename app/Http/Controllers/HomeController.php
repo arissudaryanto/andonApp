7fe1dd863fc9878f05273f34793eae4bac519b44
Aris Sudaryanto <arissudaryanto25@gmail.com>
2022-01-05 14:57:29 +0700

    fix assign hardware

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Dashboard\Sales;
 use App\Models\Module\Dashboard;
 use App\Models\Master\Hardware;
-
+use Auth;
 
 class HomeController extends Controller
 {
@@ -27,12 +27,17 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
+        $id = Auth::user()->id;
+
         $year = $request->get('year');
         if($request->get('year') == null){
             $year = date('Y');
         }
         $hardware = Dashboard::getHardware();
-        $device   = Hardware::whereNull('deleted_at')->where('status',1)->get();
+        $device   = Hardware::whereNull('deleted_at')
+        ->whereJsonContains('users', ["$id"])
+        ->where('status',1)->get();
+
         $entity   = Dashboard::getEntity($year);
         return view('dashboard.line',compact('entity','hardware','device'));
     }
